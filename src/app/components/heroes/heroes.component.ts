09b4436054b999c19c9115abfe140ea0663f3a80
agroch1 <grochowski.arek@gmail.com>
2018-12-14 11:17:14 +0100

    end

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../../hero';
-import { HEROES} from '../../mock-heroes';
 import { HeroService } from '../../services/hero.service';
-import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -11,7 +10,6 @@ import {Observable} from 'rxjs';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
@@ -19,13 +17,23 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onselect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(value => this.heroes = value);
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 
 }
